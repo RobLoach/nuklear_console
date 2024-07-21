ebc8b81c5f34de019f01aaa40ee7efe2ba9ef0b2
Rob Loach <robloach@gmail.com>
2024-07-21 19:26:42 -0400

    Clean up memory usage

--- nuklear_console_file_system.h
+++ nuklear_console_file_system.h
@@ -53,25 +53,6 @@ extern "C" {
 #endif  // NK_CONSOLE_FILE_ADD_FILES_TINYDIR_SKIP
 
 /**
- * Destroy callback; Clears the tinydir memory.
- *
- * @param console The file widget that holds the tinydir data.
- *
- * @see nk_console_file_add_files_tinydir()
- */
-static void nk_console_file_destroy_tinydir(nk_console* console) {
-    void* file_user_data = nk_console_file_get_file_user_data(console);
-    if (file_user_data == NULL) {
-        return;
-    }
-
-    // Clear the tinydir data.
-    tinydir_close((tinydir_dir*)file_user_data);
-    nk_console_mfree(nk_handle_id(0), file_user_data);
-    nk_console_file_set_file_user_data(console, NULL);
-}
-
-/**
  * Iterate through the files in the given directory, and add the contents as widgets.
  *
  * @param parent The file widget.
@@ -84,34 +65,20 @@ static void nk_console_file_add_files_tinydir(nk_console* parent, const char* di
         return;
     }
 
-    // Initialize the tinydir memory if needed.
-    tinydir_dir* dir = (tinydir_dir*)nk_console_file_get_file_user_data(parent);
-    if (dir == NULL) {
-        dir = (tinydir_dir*)nk_console_malloc(nk_handle_id(0), NULL, sizeof(tinydir_dir));
-        if (dir == NULL) {
-            return;
-        }
-        nk_console_file_set_file_user_data(parent, dir);
-
-        // Use the destructor to close tinydir.
-        parent->destroy = nk_console_file_destroy_tinydir;
-    }
-    else {
-        // Since tinydir exists already, close the active directory.
-        tinydir_close(dir);
-    }
-
-    // Open the new directory
-    if (tinydir_open_sorted(dir, directory) == -1) {
+    tinydir_dir dir;
+    if (tinydir_open_sorted(&dir, directory) == -1) {
         return;
     }
 
     // Iterate through the files and add each entry.
-    for (size_t i = 0; i < dir->n_files; i++) {
+    for (size_t i = 0; i < dir.n_files; i++) {
         tinydir_file file;
-        tinydir_readfile_n(dir, &file, i);
+        tinydir_readfile_n(&dir, &file, i);
         nk_console_file_add_entry(parent, file.name, file.is_dir == 0 ? nk_false : nk_true);
     }
+
+    // Close the directory.
+    tinydir_close(&dir);
 }
 
 // Tell the file widget to use the tinydir file system.
@@ -119,24 +86,6 @@ static void nk_console_file_add_files_tinydir(nk_console* parent, const char* di
 
 // Raylib support
 #elif defined(RAYLIB_VERSION) || defined(NK_CONSOLE_ENABLE_RAYLIB)
-
-/**
- * Destroy callback; Clear the raylib file system memory.
- *
- * @param The file widget that holds the raylib data.
- *
- * @see nk_console_file_add_files_raylib()
- */
-void nk_console_file_destroy_raylib(nk_console* console) {
-    FilePathList* list = (FilePathList*)nk_console_file_get_file_user_data(console);
-    if (list != NULL) {
-        // Clear the directory entries.
-        UnloadDirectoryFiles(*list);
-
-        // Clear our the FilePathList data.
-        MemFree(list);
-    }
-}
 
 /**
  * nuklear_console_file callback to iterate through a directory and ad all entries from the given path.
@@ -147,23 +96,23 @@ void nk_console_file_destroy_raylib(nk_console* console) {
  * @see nk_console_file_add_entry()
  */
 void nk_console_file_add_files_raylib(nk_console* console, const char* path) {
-    FilePathList* list = nk_console_file_get_file_user_data(console);
-    if (list == NULL) {
-        list = (FilePathList*)MemAlloc(sizeof(FilePathList));
-        nk_console_file_set_file_user_data(console, (void*)list);
-        console->destroy = nk_console_file_destroy_raylib;
-    }
-    else {
-        UnloadDirectoryFiles(*list);
+    FilePathList filePathList = LoadDirectoryFiles(path);
+
+    // Directories
+    for (int i = 0; i < filePathList.count; i++) {
+        if (DirectoryExists(filePathList.paths[i])) {
+            nk_console_file_add_entry(console, filePathList.paths[i], nk_true);
+        }
     }
 
-    TraceLog(LOG_INFO, "Loading files from directory: %s", path);
-    FilePathList filePathList = LoadDirectoryFiles(path);
+    // Files
     for (int i = 0; i < filePathList.count; i++) {
-        TraceLog(LOG_INFO, "  %s", filePathList.paths[i]);
-        nk_console_file_add_entry(console, filePathList.paths[i], DirectoryExists(filePathList.paths[i]));
+        if (FileExists(filePathList.paths[i])) {
+            nk_console_file_add_entry(console, filePathList.paths[i], nk_false);
+        }
     }
-    *list = filePathList;
+
+    UnloadDirectoryFiles(filePathList);
 }
 
 // Tell the file widget to use the raylib file system.
