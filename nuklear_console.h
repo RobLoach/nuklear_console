202a6ac177881dcc7029133474a32d40719ff02c
Rob Loach <robloach@gmail.com>
2024-07-21 22:35:48 -0400

    Add the Color Picker
    Fixes #52

--- nuklear_console.h
+++ nuklear_console.h
@@ -41,6 +41,7 @@ typedef enum {
     NK_CONSOLE_FILE,
     NK_CONSOLE_IMAGE,
     NK_CONSOLE_SPACING,
+    NK_CONSOLE_COLOR,
 } nk_console_widget_type;
 
 typedef struct nk_console_message {
@@ -137,6 +138,7 @@ NK_API int nk_console_height(nk_console* widget);
 #include "nuklear_console_file_system.h"
 #include "nuklear_console_image.h"
 #include "nuklear_console_spacing.h"
+#include "nuklear_console_color.h"
 #undef NK_CONSOLE_HEADER_ONLY
 
 #ifdef __cplusplus
@@ -246,6 +248,7 @@ NK_API nk_bool nk_input_is_mouse_moved(const struct nk_input* input);
 #include "nuklear_console_file.h"
 #include "nuklear_console_image.h"
 #include "nuklear_console_spacing.h"
+#include "nuklear_console_color.h"
 
 NK_API const char* nk_console_get_label(nk_console* widget) {
     if (widget == NULL) {
