514376b25d207ba385c9d276b1c0f7eb094d379e
Rob Loach <robloach@gmail.com>
2026-02-07 00:55:37 -0500

    Update dependencies

--- demo/pntr/.cmake/Findnuklear_gamepad.cmake
+++ demo/pntr/.cmake/Findnuklear_gamepad.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     nuklear_gamepad
     GIT_REPOSITORY https://github.com/RobLoach/nuklear_gamepad.git
-    GIT_TAG e46ea1c
+    GIT_TAG 985c692
 )
 FetchContent_MakeAvailable(nuklear_gamepad)
