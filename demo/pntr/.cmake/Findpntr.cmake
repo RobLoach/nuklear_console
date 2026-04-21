efbaebe86f8b8ac4f754b75b40871aa7bf4afd1a
Rob Loach <robloach@gmail.com>
2026-04-21 11:37:07 -0400

    Update pntr_nuklear

--- demo/pntr/.cmake/Findpntr.cmake
+++ demo/pntr/.cmake/Findpntr.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     pntr
     GIT_REPOSITORY https://github.com/robloach/pntr.git
-    GIT_TAG 936bd8c
+    GIT_TAG 65c426f
 )
 FetchContent_MakeAvailable(pntr)
--- demo/pntr/.cmake/Findpntr_nuklear.cmake
+++ demo/pntr/.cmake/Findpntr_nuklear.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     pntr_nuklear
     GIT_REPOSITORY https://github.com/RobLoach/pntr_nuklear.git
-    GIT_TAG a2f7028
+    GIT_TAG 3e192eb
 )
 FetchContent_MakeAvailable(pntr_nuklear)
